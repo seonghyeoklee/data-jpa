2f3d03badcb78d10f2f9ef8d71c87599b2c71eb3
seonghyeoklee <dltjdgur327@gmail.com>
2021-12-05 17:44:09 +0900

    feat : @EntityGraph 사용방법

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -238,4 +238,29 @@ class MemberRepositoryTest {
 
         assertThat(resultCount).isEqualTo(4);
     }
+
+    @Test
+    public void findMemberLazy() {
+        //member1 -> teamA
+        //member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = memberRepository.findAll();
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("team = " + member.getTeam().getName());
+        }
+    }
 }
