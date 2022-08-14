c9f8ee006eb229ee3ad58919f2f7fba7f73047c2
seonghyeoklee <dltjdgur327@gmail.com>
2022-08-14 18:11:38 +0900

    init

---
+++ spring-advanced/src/main/java/hello/advenced/SpringAdvencedApplication.java
@@ -0,0 +1,13 @@
+package hello.advenced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvencedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvencedApplication.class, args);
+    }
+
+}
