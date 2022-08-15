9c29887e8451af37290ec9d82e219a9780db3359
seonghyeoklee <dltjdgur327@gmail.com>
2022-08-15 17:04:14 +0900

    로그 추적기 v2 적용

---
+++ spring-advanced/src/main/java/hello/advenced/app/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package hello.advenced.app.v2;
+
+import hello.advenced.trace.TraceId;
+import hello.advenced.trace.TraceStatus;
+import hello.advenced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+    public void orderItem(TraceId traceId, String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+
+    }
+
+}
