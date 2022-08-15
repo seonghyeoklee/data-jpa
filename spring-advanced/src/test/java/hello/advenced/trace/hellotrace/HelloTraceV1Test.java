b68e44e686339c5d91325eb8733bbb3262953fc7
seonghyeoklee <dltjdgur327@gmail.com>
2022-08-15 16:19:37 +0900

    로그 추적기 v1 적용

---
+++ spring-advanced/src/test/java/hello/advenced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,24 @@
+package hello.advenced.trace.hellotrace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.advenced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
