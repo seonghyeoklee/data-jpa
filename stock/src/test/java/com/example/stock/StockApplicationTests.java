56ef1571e48be11c5e797cf5cf1818560a13da82
seonghyeoklee <dltjdgur327@gmail.com>
2022-09-25 00:50:29 +0900

    현재 로직으로 100개의 요청을 동시에 진행했을때 문제점

--- stock/src/test/java/com/example/stock/StockApplicationTests.java
+++ stock/src/test/java/com/example/stock/StockApplicationTests.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import com.example.stock.service.StockService;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -41,6 +44,32 @@ class StockApplicationTests {
 
         // then
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    void 동시에_100개_요청() throws Exception {
+        // given
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // then
+        assertEquals(0L, stock.getQuantity());
     }
 
 }
