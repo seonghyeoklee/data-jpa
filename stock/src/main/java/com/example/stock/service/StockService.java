3df8a7aca50252127a617112c19cccce7e77b319
seonghyeoklee <dltjdgur327@gmail.com>
2022-09-25 01:07:25 +0900

    synchronized 사용시 @Transactional 제거

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -2,6 +2,7 @@ package com.example.stock.service;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,8 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
 
