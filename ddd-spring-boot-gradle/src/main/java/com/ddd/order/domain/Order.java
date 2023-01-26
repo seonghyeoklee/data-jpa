f029eca4418d711ba75d01e3cedb7c32c253899d
seonghyeoklee <dltjdgur327@gmail.com>
2023-01-26 23:58:50 +0900

    feat : 식별자 생성 (equals(), hashCode())

--- ddd-spring-boot-gradle/src/main/java/com/ddd/order/domain/Order.java
+++ ddd-spring-boot-gradle/src/main/java/com/ddd/order/domain/Order.java
@@ -1,8 +1,10 @@
 package com.ddd.order.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
+    private String orderNumber;
     private OrderState state;
     private ShippingInfo shippingInfo;
     private List<OrderLine> orderLines;
@@ -61,5 +63,18 @@ public class Order {
 
     public void completePayment() {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(orderNumber, order.orderNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderNumber);
     }
 }
