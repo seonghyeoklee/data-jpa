df0a8601de856954b340d375e4ccaf70b95baa69
seonghyeoklee <dltjdgur327@gmail.com>
2023-02-02 21:45:27 +0900

    feat : project init

---
+++ mysql-init-project/src/main/java/com/example/mysql/hello/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.mysql.hello;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HelloWorldController {
+    final private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @GetMapping("/hello")
+    public Integer helloWorld() {
+        return jdbcTemplate.queryForObject("SELECT 1", new MapSqlParameterSource(), Integer.class);
+    }
+}
