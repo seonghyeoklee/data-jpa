3230fef8a5fa85e5eb567d5a8ff2c717686aa438
seonghyeoklee <dltjdgur327@gmail.com>
2023-02-04 00:03:57 +0900

    feat : 엔티티에서 dto 전환

--- mysql-init-project/src/main/java/com/example/mysql/controller/MemberController.java
+++ mysql-init-project/src/main/java/com/example/mysql/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.mysql.controller;
 
+import com.example.mysql.domain.member.dto.MemberDto;
 import com.example.mysql.domain.member.dto.RegisterMemberCommand;
 import com.example.mysql.domain.member.entity.Member;
 import com.example.mysql.domain.member.service.MemberReadService;
@@ -14,12 +15,13 @@ public class MemberController {
     private final MemberReadService memberReadService;
 
     @PostMapping("/members")
-    public Member register(@RequestBody RegisterMemberCommand command) {
-        return memberWriteService.create(command);
+    public MemberDto register(@RequestBody RegisterMemberCommand command) {
+        Member member = memberWriteService.create(command);
+        return memberReadService.toDto(member);
     }
 
     @GetMapping("/members/{id}")
-    public Member getMember(@PathVariable Long id) {
+    public MemberDto getMember(@PathVariable Long id) {
         return memberReadService.getMember(id);
     }
 }
--- mysql-init-project/src/main/java/com/example/mysql/domain/member/service/MemberReadService.java
+++ mysql-init-project/src/main/java/com/example/mysql/domain/member/service/MemberReadService.java
@@ -1,5 +1,6 @@
 package com.example.mysql.domain.member.service;
 
+import com.example.mysql.domain.member.dto.MemberDto;
 import com.example.mysql.domain.member.entity.Member;
 import com.example.mysql.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,12 @@ public class MemberReadService {
 
     private final MemberRepository memberRepository;
 
-    public Member getMember(Long id) {
-        return memberRepository.findById(id).orElseThrow();
+    public MemberDto getMember(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow();
+        return toDto(member);
+    }
+
+    public MemberDto toDto(Member member) {
+        return new MemberDto(member.getId(), member.getEmail(), member.getNickname(), member.getBirthday());
     }
 }
